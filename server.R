2d5e925bba8b0accac13c3e3ec3852b534c2608a
jamespbrink <james.p.brink@gmail.com>
2014-10-21 17:30:25 -0400

    tweaks

--- server.R
+++ server.R
@@ -1,17 +1,22 @@
 library(shiny)
 library(datasets)
 
+#import dataset
 airData <- airquality
 
+#make list of columns for dropdowns
 columns <- list("O Zone" = "Ozone", "Solar Radiation" = "Solar.R", "Wind" = "Wind", "Temperature" = "Temp")
 
 # Define server logic
 shinyServer(function(input, output, session) {
+        
+        #set dropdown so it only shows 3 remaining columns, won't compare a variable to itself
         output$predictor = renderUI({
                 outc <- input$outcome
                 selectInput("predict", "Predictor:", columns[-grep(outc,columns)])
         })
         
+        #setup the slider for the current selections, default to mean, set max and min to the observed max and min
         output$slider = renderUI({
                 slLabel = paste("Adjust to predict ", names(columns[grep(input$outcome,columns)]), "using this value for ", names(columns[grep(input$predict,columns)]), ":")
                 slVal = round(mean(airData[,input$predict], na.rm=TRUE),0)
@@ -20,10 +25,12 @@ shinyServer(function(input, output, session) {
                 sliderInput('slide', slLabel, value = slVal, min = slMin, max = slMax, step=1,)
         })
         
+        #create formula for plot
         formulaText <- reactive({
                 paste(input$outcome, " ~ ", input$predict)
         })
         
+        #create text for prediction
         output$guess <- renderText({
                 fit <- lm(as.formula(formulaText()), data=airData)
                 slp <- fit$coeff[2]
@@ -32,20 +39,12 @@ shinyServer(function(input, output, session) {
                 paste("Predicted outcome for ", names(columns[grep(input$outcome,columns)]), ": ", predout)
         })
         
-        output$pred <- renderText({
-                names(columns[grep(input$predict,columns)])
-        })
-        
-        
+        #create plot and fitted line
         output$ioPlot <- renderPlot({
                 fit <- lm(as.formula(formulaText()), data=airData)
-
                 plot(as.formula(formulaText()), 
                         data = airData, xlab=names(columns[grep(input$predict,columns)]), ylab=names(columns[grep(input$outcome,columns)]))
-                
                 abline(fit, col="red")
-                
                 title(main= paste(names(columns[grep(input$outcome,columns)]), " vs. ", names(columns[grep(input$predict,columns)])))
-
         })
 })
